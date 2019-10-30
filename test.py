b309b0a788b1549fc6a04e9385eee0ea7568c563
azahiri <54958911+azahiri@users.noreply.github.com>
2019-10-30 15:44:20 -0400

    update

--- test.py
+++ test.py
@@ -11,19 +11,29 @@ import seaborn as sns
 import time
 import datetime
 
-#authenticate the keys 
-consumer_key = 'UWWq3Sfkk89IPHiMIW3geSgXS'
-consumer_secret = '9YcxuR1QoxS81FEEnY7FpA76G98j3q31fhl7MT0i55rFMGmV7W'
-access_token = '1041370203563732992-pnt2lwFytx0JBfaC3Mgpr9XlZ60m7F' 
-acccess_token_secret = 'D2PqFSBeLzjHelCqMKHvipr5vHcWhkdCJH9S4CWGsQkwS'
+# We import our access keys:
+from credentials import *    # This will allow us to use the keys as variables
+
+# API's setup:
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with our access keys provided.
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+# We create an extractor object:
+extractor = twitter_setup()
 
 KEYWORDS = ['Arsenal', 'Emery', 'Xhaka', 'Ozil']
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, acccess_token_secret)
-api = tweepy.API(auth)
-
-tweets = tweepy.Cursor(api.user_timeline, screen_name='piersmorgan')
+tweets = tweepy.Cursor(extractor.user_timeline, screen_name='piersmorgan')
 tweet_list = list(tweets.items())
 
 def process_tweets(tweets, keywords):
@@ -44,12 +54,6 @@ useful_tweets = process_tweets(tweet_list, KEYWORDS)
 # print(useful_tweets)
 # print(len(useful_tweets))    
 
-
-def show_count(tweets):
-    count = 0
-    for tweet in tweets:
-        count += 1
-    return count
 
 def show_text(tweets):
     arsenal_tweet_text = []
@@ -106,6 +110,8 @@ useful_tweets_source = show_source(useful_tweets)
 useful_tweets_likes = show_likes(useful_tweets)
 useful_tweets_RT = show_RT(useful_tweets)
 
+print("Total number of tweets about Arsenal: \n{} tweets.".format(len(useful_tweets)))
+
 # # We create a pandas dataframe as follows:
 data = pd.DataFrame(data=useful_tweets_text, columns=['Tweets'])
 
@@ -123,7 +129,7 @@ display(data.head(10))
 # # We extract the mean of lenghts:
 mean = np.mean(data['len'])
 
-print("The lenght's average in tweets: {}".format(int(mean)))
+print("Average length of tweet: {} characters.".format(int(mean)))
 
 # We extract the tweet with more FAVs and more RTs:
 
@@ -181,6 +187,45 @@ for source in data['Source']:
 percent /= 100
 
 # Pie chart:
-pie_chart = pd.Series(percent, index=sources, name='Sources')
-pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6))
-plt.show()
+# pie_chart = pd.Series(percent, index=sources, name='Sources')
+# pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6))
+# plt.show()
+
+from textblob import TextBlob
+import re
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing 
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
+# We create a column with the result of the analysis:
+data['SA'] = np.array([ analize_sentiment(tweet) for tweet in data['Tweets'] ])
+
+# We display the updated dataframe with the new column:
+display(data.head(10))
+
+# We construct lists with classified tweets:
+pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] > 0]
+neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == 0]
+neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] < 0]
+
+# We print percentages:
+print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['Tweets'])))
+print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['Tweets'])))
+print("Percentage of negative tweets: {}%".format(len(neg_tweets)*100/len(data['Tweets'])))
